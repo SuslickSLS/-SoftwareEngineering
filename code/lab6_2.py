cc5a745e73f8f5ebebf19fc68c1e57dd70e3dbfe
SuslickSLS <109274064+SuslickSLS@users.noreply.github.com>
2024-04-05 01:52:08 +0500

    Update lab6_2.py

--- code/lab6_2.py
+++ code/lab6_2.py
@@ -1,1 +1,18 @@
+def removeNumber(str):
+    tup = tuple(map(int,str[:-4].strip('()').split(',')))
+    element = int(str[-2])
+    newTuple = tup
 
+    if element in tup:
+        index = tup.index(element)
+        newTuple = tup[:index] + tup[index + 1:]
+
+    return  tuple(newTuple)
+
+one = '(1, 2, 3), 1)'
+two = '(1, 2, 3, 1, 2, 3, 4, 5, 2, 3, 4, 2, 4, 2), 3)'
+three = '(2, 4, 6, 6, 4, 2), 9)'
+
+print("one - ", removeNumber(one))
+print("two - ", removeNumber(two))
+print("three - ", removeNumber(three))
